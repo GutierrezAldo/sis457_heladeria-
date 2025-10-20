4eed15d50c08ebbe6995a017fe4071a607b495ee
Elizabeth <elizabethdiazcanchari@gmail.com>
2025-10-20 09:42:16 -0400

    feat: Implementar solucion y crear Cp, Cad y Cln

---
+++ Sis457Heladeria/ClnHeladeria/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClnHeladeria
+{
+    public class Class1
+    {
+    }
+}
